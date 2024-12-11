33d21e68107f485f4bb0c5c6b6926bcde16a530a
melihx <melymh21@gmail.com>
2024-12-11 20:58:54 +0200

    Initial commit

---
+++ src/main/java/uni/fmi/car_management_api/CarManagementApiApplication.java
@@ -0,0 +1,13 @@
+package uni.fmi.car_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uni/fmi/car_management_api/CarManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.fmi.car_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
